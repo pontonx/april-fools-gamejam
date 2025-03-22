d6f45201257d819f6dcc6abb66ed96ff69c25663
ponton <31003388+pontonx@users.noreply.github.com>
2025-03-22 18:20:48 +0100

    Implemented Clock

---
+++ Assets/ClockItem.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockItem : MonoBehaviour, InteractableInterface
+{
+    [SerializeField] private GameObject clockObject;
+
+    public string interactableText = "Pick up";
+    public string InteractableText
+    {
+        get => interactableText;
+        set => interactableText = value;
+    }
+
+    public void Interact()
+    {
+        clockObject.SetActive(true);
+        Destroy(this.gameObject);
+    }
+}
