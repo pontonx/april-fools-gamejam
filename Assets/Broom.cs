d5e0f1d87788c5269a59b0933cedf714027ae6ce
ponton <31003388+pontonx@users.noreply.github.com>
2025-03-23 16:27:03 +0100

    Broom Functionality

--- Assets/Broom.cs
+++ Assets/Broom.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Broom : MonoBehaviour
 {
+    [SerializeField] private List<GameObject> objectsToClean;
+
     private void Update()
     {
         if(Input.GetMouseButtonDown(0))
@@ -18,12 +20,20 @@ public class Broom : MonoBehaviour
                 }
             }
         }
+
+        if(objectsToClean.Count <= 0)
+        {
+            TaskManager.instance.RemoveTask();
+            TaskManager.instance.AddTask("Make a coffee");
+            Destroy(this);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Dirt")
         {
+            objectsToClean.Remove(other.gameObject);
             Destroy(other.gameObject);
         }
     }
