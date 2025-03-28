14ce8c621017886c43848f8e91bbf12e5b86884c
ponton <31003388+pontonx@users.noreply.github.com>
2025-03-28 21:40:27 +0100

    Computer & Lightning

--- Assets/RotateMonitor.cs
+++ Assets/RotateMonitor.cs
@@ -1,18 +1,25 @@
 using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
-public class RotateMonitor : MonoBehaviour
+public class RotateMonitor : MonoBehaviour, InteractableInterface
 {
-    // Start is called before the first frame update
-    void Start()
+    public string interactableText = "Rotate";
+    public string InteractableText
     {
-        
+        get => interactableText;
+        set => interactableText = value;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Interact()
     {
-        
+        StartCoroutine(Rotate());
+    }
+
+    IEnumerator Rotate()
+    {
+        transform.DOLocalRotate(new Vector3(-90f, 0f, 0f), 1f);
+        yield return new WaitForSeconds(1f);
+        Destroy(this);
     }
 }
